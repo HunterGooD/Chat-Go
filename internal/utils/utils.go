d0020b6c8813f1c10a27eb6a67dfc0f6b02f35e9
Gudov Vladislav <vlad.gudov@inbox.ru>
2021-02-02 19:49:19 +0400

    каркас на валидацию

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,1 +1,34 @@
 package utils
+
+// ValidLogin проверка валидности логина
+func ValidLogin(login string) bool {
+	return false
+}
+
+// ValidName проверка валидности имени
+func ValidName(name string) bool {
+	return false
+}
+
+// ValidPassword проверка валидности пароля
+func ValidPassword(password string) bool {
+	return false
+}
+
+// ValidateUserData проверка валидности всех параметров пользователя
+func ValidateUserData(userData map[string]string) bool {
+	login, ok := userData["login"]
+	if !ok {
+		return false
+	}
+	name, ok := userData["name"]
+	if !ok {
+		return false
+	}
+	passw, ok := userData["password"]
+	if !ok {
+		return false
+	}
+
+	return ValidLogin(login) && ValidName(name) && ValidPassword(passw)
+}
